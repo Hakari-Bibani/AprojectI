e53434b238cc7a967433758d7a61de79e5291a00
Hakari Bibani <hakary.jalal33@gmail.com>
2025-02-09 17:15:38 +0300

    Update style1.py

--- utils/style1.py
+++ utils/style1.py
@@ -1,1 +1,21 @@
+# utils/style1.py
+import streamlit as st
+
+def set_page_style():
+    """
+    A placeholder style function. 
+    You can adjust or add custom CSS here.
+    """
+    st.markdown(
+        """
+        <style>
+        /* Example custom styling */
+        body {
+            background-color: #121212;
+            color: #fff;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
