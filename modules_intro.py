1ca4b00f5cdf0c8f619fd22e8eea01694e8d2391
Hakari Bibani <hakary.jalal33@gmail.com>
2025-02-09 15:37:40 +0300

    Update modules_intro.py

--- modules_intro.py
+++ modules_intro.py
@@ -24,10 +24,11 @@ def show():
 
         <h2 style='color: #FFD3A3;'>Online Sessions:</h2>
         * To schedule one-on-one sessions, stay in contact with the instructor for organizing a convenient time.
+        
         * These sessions are designed to address your queries, provide feedback, and guide you through challenges.
 
         <h2 style='color: #FFD3A3;'>Assignment Submission:</h2>
-        * **Submit assignments promptly and ensure all required files and links are included.**
+        Submit assignments promptly and ensure all required files and links are included.
         
         * **For Google Colab projects**, ensure they are **set to public** so your peers and the instructor can access and review them.
         """, unsafe_allow_html=True)
